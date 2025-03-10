7c9b2f37f25ea42b9f586257895d63564f31903a
Balajireddyk4361 <balajireddyk4451@gmail.com>
2025-03-10 11:43:36 +0530

    datatypes in python

---
+++ datatypes.py
@@ -0,0 +1,54 @@
+# eid=101
+# esal=400000.45
+# ename="balaji"
+# avail="b"
+# c=2+2j
+# discount=True
+# print(type(eid))
+# print(type(esal))
+# print(type(ename))
+# print(type(avail))
+# print(type(discount))
+# print(type(c))
+
+
+#create vairable
+eid=101
+esal=45000.45
+ename="Rahul"
+avail='N'
+c = 10+20j
+discount=True 
+
+eids=[101,102,103,104]
+enames=('Rahul','Sonia','Priyanka')
+numbers={10,20,30,10,20,30}
+emp={
+    'eid':101,
+    'ename':'Rahul'
+}
+q=bytes([10,20,30,40,255])
+b=bytes([10,20,30,255])
+ba=bytearray([10,20,30,255])
+fz=frozenset({10,20,30,255,10,20,30,255})
+r=range(0,100)
+#print Data type
+print(type(eid))
+print(type(esal))
+print(type(ename))
+print(type(avail))
+print(type(c))
+print(type(discount))
+print(type(eids))
+print(type(enames))
+print(type(numbers))
+print(type(emp))
+print(type(q))
+print(q)
+print(b)   
+print(ba)
+print(type(fz))
+print(type(q))
+print(type(ba))
+print (r)
+print (type(r))
